2fd4ecec65bd4794805ad60aa0e35816be09fdf0
N3X-AGENCY <n3x503@gmail.com>
2024-08-22 20:40:42 +0700

    Better readbility

--- src/routes/call/ReasonSelector.svelte
+++ src/routes/call/ReasonSelector.svelte
@@ -25,12 +25,12 @@
 </script>
 
 <div class="bg-black bg-opacity-20 border border-white border-opacity-10 rounded-sm p-6" in:fade={{duration: 200}}>
-  <h3 class="text-lg font-semibold mb-4 text-white">Reason for Call</h3>
+  <h3 class="text-lg font-semibold mb-4 text-white">À quel sujet ?</h3>
   <div class="grid grid-cols-3 gap-3 mb-6">
     {#each Object.keys(categories) as category}
       <button
         type="button"
-        class="p-3 text-sm rounded-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-30 text-white transition-all duration-200"
+        class= "{selectedCategory === category ? 'bg-white/40 text-black' : 'bg-white/10'} rounded-lg p-3 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-30 text-white transition-all duration-200"
         class:bg-white={selectedCategory === category}
         class:bg-opacity-20={selectedCategory === category}
         class:text-black={selectedCategory === category}
@@ -45,14 +45,13 @@
   
   {#if selectedCategory}
     <div class="mt-6" in:slide={{duration: 200}}>
-      <h4 class="text-md font-semibold mb-3 text-white text-opacity-80">Select Subcategory</h4>
+      <h4 class="text-md font-semibold mb-3 text-white text-opacity-80">Dit moi en plus</h4>
       <div class="grid grid-cols-2 gap-3 max-h-48 overflow-y-auto pr-2 custom-scrollbar">
         {#each categories[selectedCategory as keyof typeof categories] as subcategory}
           <button
             type="button"
-            class="p-3 text-sm rounded-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-30 text-white transition-all duration-200"
-            class:bg-white={selectedSubcategory === subcategory}
-            class:bg-opacity-20={selectedSubcategory === subcategory}
+            class=" rounded-lg p-3 text-sm {selectedSubcategory === subcategory ? 'bg-white/40 text-black' : 'bg-white/10'} focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-30 text-white transition-all duration-200"
+           
             class:text-black={selectedSubcategory === subcategory}
             class:hover:bg-white={selectedSubcategory !== subcategory}
             class:hover:bg-opacity-10={selectedSubcategory !== subcategory}
